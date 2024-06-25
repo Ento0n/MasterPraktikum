bf8904c6246283021384769015f44ba3f1fd07bc
Anton <ge54kog@mytum.de>
2024-06-25 15:07:38 +0200

    Add stuff for the first synteny plot and new plot for presentation wednesday

--- src/tinkering/graphs/graph.py
+++ src/tinkering/graphs/graph.py
@@ -2,7 +2,8 @@ import plotly.graph_objects as go
 
 # Define the x values and original y values
 x_values = ["UniProt", "GenBank accession", "GenBank CDSs", "Correct reconstructed"]
-original_y_values = [15224, 15201, 15155, 12321]
+# original_y_values = [15224, 15201, 15155, 12321]
+original_y_values = [15224, 15201, 15155, 14469]
 
 # Convert the y values to percentages
 y_values = [value / original_y_values[0] * 100 for value in original_y_values]
@@ -21,11 +22,13 @@ fig.add_trace(go.Scatter(
     marker=dict(size=8)
 ))
 
+fig.update_yaxes(rangemode="tozero")
+
 # Update the layout to remove y-axis values, title, and x-axis description
 fig.update_layout(
     title='',
     xaxis_title='',
-    yaxis=dict(showticklabels=False),
+    # yaxis=dict(showticklabels=False),
     xaxis=dict(tickmode='array', tickvals=list(range(len(x_values))), ticktext=x_values),
     template='plotly_white'
 )
--- src/tinkering/graphs/test_pyGenomeViz.py
+++ src/tinkering/graphs/test_pyGenomeViz.py
@@ -1,16 +1,37 @@
 from pygenomeviz import GenomeViz
 from pygenomeviz.parser import Gff
 
-gff = Gff("data/sample.gff")
+gff = Gff("data/sample_data/UCHL1_human.gff")
 
 gv = GenomeViz()
 
-# segment adapted to range of UCHL1 gene
-track = gv.add_feature_track(name=gff.name, segments=(41256928, 41268455))
-
-# plot cds features
+track = gv.add_feature_track("UCHL1 on human chromosome 4", (41256928 - 3000, 41268455 + 1000))
 cds_features = gff.extract_features(feature_type="CDS")
 track.add_features(cds_features)
 track.add_sublabel()
+
+gff = Gff("data/sample_data/UCHL1_mouse.gff")
+
+track = gv.add_feature_track("UCHL1 on mouse chromosome 5", (66833464 - 3000, 66844577 + 1000))
+cds_features = gff.extract_features(feature_type="CDS")
+track.add_features(cds_features)
+track.add_sublabel()
+
+gff = Gff("data/sample_data/BAP1_human.gff")
+
+track = gv.add_feature_track("BAP1 on human chromosome 3", (52401008 - 3000, 52410008 + 1000))
+cds_features = gff.extract_features(feature_type="CDS")
+track.add_features(cds_features)
+track.add_sublabel()
+
+gff = Gff("data/sample_data/BAP1_mouse.gff")
+
+track = gv.add_feature_track("BAP1 on mouse chromosome 14", (30973446 - 3000, 30981886 + 1000))
+cds_features = gff.extract_features(feature_type="CDS")
+track.add_features(cds_features)
+track.add_sublabel()
+
+gv.add_link(("UCHL1 on human chromosome 4", 41256977, 41268073), ("UCHL1 on mouse chromosome 5", 66833633, 66844271))
+gv.add_link(("BAP1 on human chromosome 3", 52409878, 52402288), ("BAP1 on mouse chromosome 14", 30973575, 30980796))
 
 gv.savefig("gff_sample_features.png")
